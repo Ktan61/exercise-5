d84a081eecde93a688c08c731b4794d33c73b5b3
KathrynTan <ktan61@my.bcit.ca>
2024-04-15 11:06:25 -0700

    Updating chart

--- .history/pages/index_20240415110431.js
+++ .history/pages/index_20240415110431.js
@@ -7,6 +7,9 @@ export default function Home() {
   return (
     <>
       <main className={`${styles.main}`}>
+
+        <HorizontalChart/>
+        <VerticalChart/>
         <PieChart/>
       </main>
     </>
